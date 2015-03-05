18d42874c7d83b36ebadbdbe919b95371e8adb8a
Alyssa Rosenzweig <bobbybee@users.noreply.github.com>
2015-03-05 09:38:00 -0500

    JSON decoding

--- Assets/getProductByName.cs
+++ Assets/getProductByName.cs
@@ -1,13 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using SimpleJSON;
+
+public class ProductInfo {
+	public string name;
+	public int id, aisle, aislePosition;
+
+	public ProductInfo(string _name, int _id, int _aisle, int _aislePosition) {
+		name = _name;
+		id = _id;
+		aisle = _aisle;
+		aislePosition = _aislePosition;
+	}
+}
 
 public class getProductByName : MonoBehaviour {
 	public static string endpoint = "http://localhost:8080/api/getProductByName?name=";
 
-	public static IEnumerator fetch() {
-		WWW www = new WWW(endpoint + "potato");
-		Debug.Log ("Fetching");
+	public static IEnumerator fetch(string name, System.Action<ProductInfo> result) {
+		WWW www = new WWW(endpoint + name);
 		yield return www;
-		Debug.Log(www.text);
+
+		JSONNode node = JSON.Parse(www.text);
+
+		int status = node["status"].AsInt;
+		if(status == 0) {
+			JSONNode product = node["product"];
+			int id = product["name"].AsInt;
+			int aisle = product["aisle"].AsInt;
+			int aislePosition = product["aislePosition"].AsInt;
+
+			result(new ProductInfo(name, id, aisle, aislePosition));
+		} else {
+			result(null);
+		}
 	}
 }
--- Assets/productSearch.cs
+++ Assets/productSearch.cs
@@ -4,6 +4,10 @@ using System.Collections;
 public class productSearch : MonoBehaviour {
 	IEnumerator Start() {
 		Debug.Log ("Search");
-		yield return StartCoroutine(getProductByName.fetch());
+
+		yield return StartCoroutine(getProductByName.fetch("potato", info => {
+			Debug.Log (info.name+" is on aisle "+info.aisle);
+		}));
+
 	}
 }
