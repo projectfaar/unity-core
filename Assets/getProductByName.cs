541e8a5dc7cf87e32af690312476bae941be5195
Alyssa Rosenzweig <bobbybee@users.noreply.github.com>
2015-03-05 09:23:00 -0500

    product search

---
+++ Assets/getProductByName.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class getProductByName : MonoBehaviour {
+	public static string endpoint = "http://localhost:8080/api/getProductByName?name=";
+
+	public static IEnumerator fetch() {
+		WWW www = new WWW(endpoint + "potato");
+		Debug.Log ("Fetching");
+		yield return www;
+		Debug.Log(www.text);
+	}
+}
---
+++ Assets/productSearch.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class productSearch : MonoBehaviour {
+	IEnumerator Start() {
+		Debug.Log ("Search");
+		yield return StartCoroutine(getProductByName.fetch());
+	}
+}
