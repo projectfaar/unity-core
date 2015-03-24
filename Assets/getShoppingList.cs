5bb9dbd522ea0a70929b8034bce83e0462fd4051
Alyssa Rosenzweig <bobbybee@users.noreply.github.com>
2015-03-24 13:08:44 -0400

    getShoppingList

---
+++ Assets/getShoppingList.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using SimpleJSON;
+using System;
+
+
+public class getShoppingList : MonoBehaviour {
+	public static string endpoint = "http://59a1eab1.ngrok.com/api/getShoppingList?token=";
+	
+	public static IEnumerator fetch(string name, System.Action<arr> result) {
+		WWW www = new WWW(endpoint + name);
+		yield return www;
+		
+		JSONNode node = JSON.Parse(www.text);
+
+		int status = node["status"].AsInt;
+		if(status == 0) {
+			JSONNode list = node["list"];
+			JSONArray arr = list.AsArray;
+
+			result(arr);
+		} else {
+			result(null);
+		}
+	}
+}
